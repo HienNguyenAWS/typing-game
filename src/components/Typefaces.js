c38cafc74f3cfb1c524d794d10548310e0d60362
Hien Nguyen <Hien.NguyenChat@nashtechglobal.com>
2021-12-02 15:53:25 +0700

    Add project

---
+++ src/components/Typefaces.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Button from './Button';
+
+const Typefaces = (props) => {
+
+    const {
+        newWord,
+        inputValue,
+        setInputValue,
+        disabled,
+        time,
+        animation,
+        handleInput,
+        handleStart
+    } = props
+
+    return (
+        <div className="typeRacer">
+            <div className="wordOutput">
+                <p>{newWord}</p>
+            </div>
+            <div  className="time">
+                <p style={{ animation: animation != null ? animation : "" }}>{time}</p>
+            </div>
+            <div className="wordValues">
+                <input
+                    type="text"
+                    disabled={disabled && disabled}
+                    value={inputValue}
+                    onKeyPress={(e) => handleInput(e)}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    placeholder={disabled ? "" : "Start typing..."}
+                 />
+                <Button handleStart={handleStart} disabled={disabled } />
+            </div>
+        </div>
+    )
+}
+
+export default Typefaces;
